052e98325d15baa1a425ed84ac4605563d040445
Joa <joa@deininger.name>
2024-12-18 12:29:34 +0100

    Removal of leftover debugging code

--- src/webdav/xml.py
+++ src/webdav/xml.py
@@ -1,10 +1,3 @@
-import sys
-import os
-
-project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
-sys.path.insert(0, project_root)
-
-
 from typing import Optional
 
 from proj_types.proto_error import ProtocolError
